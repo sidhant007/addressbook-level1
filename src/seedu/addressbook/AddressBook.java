c778b9feaa3231e2abfa443e2f2d0fec36f0faf2
sidhant <sidhbansal@gmail.com>
2018-08-28 16:35:47 +0800

    Change find to match with phone and email as well

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -486,7 +486,10 @@ public class AddressBook {
         final ArrayList<String[]> matchedPersons = new ArrayList<>();
         for (String[] person : getAllPersonsInAddressBook()) {
             final Set<String> wordsInName = new HashSet<>(splitByWhitespace(getNameFromPerson(person)));
-            if (!Collections.disjoint(wordsInName, keywords)) {
+            final Set<String> unionOfAllInfo = new HashSet<>(wordsInName);
+            unionOfAllInfo.add(getEmailFromPerson(person));
+            unionOfAllInfo.add(getPhoneFromPerson(person));
+            if (!Collections.disjoint(unionOfAllInfo, keywords)) {
                 matchedPersons.add(person);
             }
         }
